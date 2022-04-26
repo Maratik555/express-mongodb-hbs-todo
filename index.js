bd97ca6c6456bda59fca9f952fb54dab83ec23a4
Маратик555 <amr404@bk.ru>
2022-04-26 04:11:29 +0300

    ok

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const exphbs = require('express-handlebars')
+const todoRoutes = require('./routes/todos')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(todoRoutes)
+
+async function start() {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/555", {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+    });
+    app.listen(PORT, () => {
+      console.log("Server has been started...");
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+const dbConnection = mongoose.connection;
+dbConnection.on('error', err => console.log(`Connection error: ${err}`));
+dbConnection.once('open', () => console.log('Connected to DB!'));
+
+start()
